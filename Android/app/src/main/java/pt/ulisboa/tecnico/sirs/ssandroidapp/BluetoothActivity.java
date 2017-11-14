fb2cd486a0b046987261ab906bdd28c1a75f9b71
Nuno Santos <nuno_913@hotmail.com>
2017-11-14 22:26:59 +0000

    Bluetooth connection added. Fixed bugs with scan.

--- Android/app/src/main/java/pt/ulisboa/tecnico/sirs/ssandroidapp/BluetoothActivity.java
+++ Android/app/src/main/java/pt/ulisboa/tecnico/sirs/ssandroidapp/BluetoothActivity.java
@@ -3,6 +3,7 @@ package pt.ulisboa.tecnico.sirs.ssandroidapp;
 import android.app.AlertDialog;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.DialogInterface;
@@ -10,20 +11,26 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.CompoundButton;
 import android.widget.ListView;
-import android.widget.TextView;
-import android.widget.ToggleButton;
+import android.widget.Switch;
 
+import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Set;
+import java.util.UUID;
 
 public class BluetoothActivity extends AppCompatActivity {
 
-    ArrayList<String> devices = new ArrayList<String>();
+    ArrayList<BluetoothDevice> devices = new ArrayList<BluetoothDevice>();
+    ArrayList<String> devices_description = new ArrayList<String>();
     ArrayAdapter<String> adapter;
+    BluetoothDevice chosen_device;
+    BluetoothSocket btSocket;
+    BluetoothAdapter mBluetoothAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,12 +39,13 @@ public class BluetoothActivity extends AppCompatActivity {
 
         ListView list = (ListView) findViewById(R.id.devices_list);
 
-        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, devices);
+        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_selectable_list_item, devices_description);
         list.setAdapter(adapter);
 
-        final BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+
+        //if the device doesnt have bluetooth
         if (mBluetoothAdapter == null) {
-            //if the device doesnt have bluetooth
             new AlertDialog.Builder(this)
                     .setTitle("Not compatible")
                     .setMessage("Your phone does not support Bluetooth")
@@ -51,54 +59,123 @@ public class BluetoothActivity extends AppCompatActivity {
         }
 
         int REQUEST_ENABLE_BT = 1;
+        //If bluetooth is not enabled, request to enable
         if (!mBluetoothAdapter.isEnabled()) {
             Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
             startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
         }
 
+        //if the user doesnt enable the bluetooth
         if (REQUEST_ENABLE_BT == 0){
-            //if the user doesnt enable the bluetooth
             Intent intent = new Intent(this, MainActivity.class);
             startActivity(intent);
         }
 
-        ToggleButton scan = (ToggleButton) findViewById(R.id.scan_button);
+        //set listener for the scan button
+        Switch scan = (Switch) findViewById(R.id.scan_button);
         scan.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
-                if (isChecked) {
-                    adapter.clear();
-                    registerReceiver(mReceiver, filter);
-                    mBluetoothAdapter.startDiscovery();
+            IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
+            if (isChecked) {
+                adapter.clear();
+                //add the already paired devices
+                for (BluetoothDevice device : mBluetoothAdapter.getBondedDevices()) {
+                    if (!devices.contains(device)) {
+                        devices.add(device);
+                        devices_description.add(device.getName() + " at " + device.getAddress());
+                        adapter.notifyDataSetChanged();
+                    }
+                }
+                registerReceiver(mReceiver, filter);
+                //find new devices
+                mBluetoothAdapter.startDiscovery();
+            } else {
+                unregisterReceiver(mReceiver);
+                mBluetoothAdapter.cancelDiscovery();
+            }
+            }
+        });
+
+        //set listener for the items in the list of devices
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+            @Override
+            public void onItemClick(AdapterView<?> adapter,View v, int position, long id){
+                String clickedDevice = (String) adapter.getItemAtPosition(position);
+                for (BluetoothDevice d : devices){
+                    if (d.getName().equals(clickedDevice.split(" ")[0])) {
+                        chosen_device = d;
+                        break;
+                    }
+                }
+                boolean connected = connect();  //try to connect to chosen device
+                if (connected) {
+                    Intent intent = new Intent(getBaseContext(), QRScannerActivity.class);
+                    startActivity(intent);
                 } else {
-                    unregisterReceiver(mReceiver);
-                    mBluetoothAdapter.cancelDiscovery();
+                    Intent intent = new Intent(getBaseContext(), MainActivity.class);
+                    startActivity(intent);
                 }
             }
         });
+
     }
 
-    // Create a BroadcastReceiver for ACTION_FOUND
+    //This discovers the devices nearby
     private final BroadcastReceiver mReceiver = new BroadcastReceiver() {
         public void onReceive(Context context, Intent intent) {
-            String action = intent.getAction();
-            // When discovery finds a device
-            if (BluetoothDevice.ACTION_FOUND.equals(action)) {
-                // Get the BluetoothDevice object from the Intent
-                BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
-                // Add the name and address to an array adapter to show in a ListView
-                String device_description = device.getName() + " at " + device.getAddress();
-                if (!devices.contains(device_description)) {
-                    devices.add(device_description);
-                    adapter.notifyDataSetChanged();
-                }
+        String action = intent.getAction();
+        // When discovery finds a device
+        if (BluetoothDevice.ACTION_FOUND.equals(action)) {
+            // Get the BluetoothDevice object from the Intent
+            BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+            if (!devices.contains(device)) {
+                devices.add(device);
+                devices_description.add(device.getName() + " at " + device.getAddress());
+                adapter.notifyDataSetChanged();
             }
+        }
         }
     };
 
-    public void changeNextActivity(View view) {
-        //for now the activity after the bluetooth is the main
-        Intent intent = new Intent(this, MainActivity.class);
-        startActivity(intent);
+    public boolean connect(){
+        try {
+            //create socket
+            btSocket = chosen_device.createRfcommSocketToServiceRecord(UUID.fromString("94f39d29-7d6d-437d-973b-fba39e49d4ee"));
+        } catch (IOException e) {
+            Log.d("Socket_Creation","Could not create socket.");
+            return false;
+        }
+
+        try {
+            btSocket.connect();
+        } catch(IOException e) {
+            Log.d("Connection_Failed","Could not connect to device.");
+            try {
+                btSocket.close();
+                return false;
+            } catch(IOException close) {
+                Log.d("Failed_Close", "Could not close connection");
+                return false;
+            }
+        }
+
+        //Code to test connection
+        /*
+        try {
+            btSocket.getOutputStream().write("City of stars".getBytes());
+        } catch (IOException e) {
+            Log.d("Failed_Write", "Could not write to socket.");
+            return false;
+        }
+        try {
+            byte[] buffer = new byte[1024];
+            int bytes = btSocket.getInputStream().read(buffer);
+            String readMessage = new String(buffer, 0, bytes);
+            Log.d("Thomas message", readMessage);
+        } catch (IOException e) {
+            Log.d("Failed_Read", "Could not read from socket.");
+            return false;
+        }*/
+        return true;
     }
 }
