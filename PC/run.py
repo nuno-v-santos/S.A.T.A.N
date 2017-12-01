ff110b2a87f4f5094f07b177a1601b66336defe0
Tomás Cunha <tomas.cunha@tecnico.ulisboa.pt>
2017-12-01 17:24:19 +0000

    Delete run.py

--- PC/run.py
+++
@@ -1,9 +0,0 @@
-import runpy
-import sys
-import os
-
-
-path = os.path.dirname(sys.modules[__name__].__file__)
-path = os.path.join(path, '..')
-sys.path.insert(0, path)
-runpy.run_module('secure-pc', run_name="__main__", alter_sys=True)
