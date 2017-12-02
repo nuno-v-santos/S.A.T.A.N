19a4b3891e4739309e1d8d2bfba861140988cc36
Tomás Cunha <tomas.cunha@tecnico.ulisboa.pt>
2017-12-02 20:48:39 +0000

    Change main to test initial key exchange

--- PC/secure-pc/main.py
+++ PC/secure-pc/main.py
@@ -1,8 +1,9 @@
 import logging
-
-from Cryptodome.PublicKey import RSA
+import qrcode
 
 from messaging.communication import BluetoothCommunication
+from security.keys import RSAKeyManager
+from security.encryption import RSAEncryption, AES256Encryption
 
 
 def main():
@@ -10,15 +11,34 @@ def main():
     # c = Controller()
     # app.MainLoop()
     logging.getLogger().setLevel(logging.DEBUG)
-    key = RSA.generate(2048).publickey().exportKey()
-    logging.debug("Starting bluetooth interface...")
+    logging.debug("Generating RSA Key Pair")
+    key_pair = RSAKeyManager().create_key_pair(2048)
+    rsa_private_cipher = RSAEncryption(key_pair.private_key)
+    logging.debug("Starting bluetooth interface")
     with BluetoothCommunication() as bluetooth_interface:
         bluetooth_interface.accept()
-        logging.debug("Receiving message...")
-        logging.debug(bluetooth_interface.receive(4096))
-        msg = key
-        bluetooth_interface.send(msg)
+        logging.debug("Creating QR Code with public key")
+        qr = qrcode.make(key_pair.public_key.exportKey())
+        qr.show()
 
+        logging.debug("Receiving session key")
+        session_key = bluetooth_interface.receive(256)
+        logging.debug("Decrypting session key")
+        session_key = rsa_private_cipher.decrypt(session_key)
+        aes_cipher = AES256Encryption(session_key, mode=AES256Encryption.MODE_CBC)
 
+        logging.debug("Receiving phone's public key")
+        phone_key = bluetooth_interface.receive(2048)
+        iv, phone_key = phone_key[:16], phone_key[16:]
+        logging.debug("Decrypting phone's public key")
+        phone_key = aes_cipher.decrypt(phone_key, iv=iv)
+        logging.debug("Phone key is {}".format(phone_key))
+
+        logging.debug("Receiving Disk Encryption Key")
+        disk_key = bluetooth_interface.receive(2048)
+        logging.debug("Decrypting Disk Encryption Key")
+        iv, disk_key = disk_key[:16], disk_key[16:]
+        disk_key = aes_cipher.decrypt(disk_key, iv=iv)
+        logging.debug("Disk Encryption Key (encrypted by Master Encryption Key) is {}".format(disk_key.hex()))
 if __name__ == '__main__':
     main()
