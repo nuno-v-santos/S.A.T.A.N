2f6383491bcf6c1e00581aedb0de6389aeec00be
Nuno Santos <nuno_913@hotmail.com>
2017-12-02 16:29:05 +0000

    Updated encryption interface.

--- Android/app/src/main/java/pt/ulisboa/tecnico/sirs/ssandroidapp/Security/EncryptionInterface.java
+++ Android/app/src/main/java/pt/ulisboa/tecnico/sirs/ssandroidapp/Security/EncryptionInterface.java
@@ -1,11 +1,15 @@
 package pt.ulisboa.tecnico.sirs.ssandroidapp.Security;
 
+import java.security.Key;
+
 /**
  * Created by Nuno Santos on 30/11/2017.
  */
 
 public interface EncryptionInterface {
 
-    byte[] encrypt(byte[] message);
-    byte[] decrypt(byte[] message);
+    byte[] AESencrypt(byte[] message, Key key, String mode, byte[] iv);
+    byte[] AESdecrypt(byte[] message, Key key, String mode, byte[] iv);
+    byte[] RSAencrypt(byte[] message, Key key);
+    byte[] RSAdecrypt(byte[] message, Key key);
 }
