2effb60c36f7d3d9b82891a08487d77e179cabc5
Guilherme <guilherme.j.santos@tecnico.ulisboa.pt>
2017-12-02 18:14:59 +0000

    #2 Key exchange activity almost completed

--- Android/app/src/main/java/pt/ulisboa/tecnico/sirs/ssandroidapp/Constants.java
+++ Android/app/src/main/java/pt/ulisboa/tecnico/sirs/ssandroidapp/Constants.java
@@ -7,13 +7,14 @@ package pt.ulisboa.tecnico.sirs.ssandroidapp;
 public class Constants {
     public static final String COMPUTER_OBJ = "pt.ulisboa.tecnico.sirs.ssandroidapp.Computer";
     public static final String COMPUTER_OBJ_FILENAME = "computer.obj";
-    public static final String COMPUTER_PUBLIC_KEY_ID = "pt.ulisboa.tecnico.sirs.ssandroidapp.publicKey";
+    public static final String COMPUTER_PUBLIC_KEY_ID = "pt.ulisboa.tecnico.sirs.ssandroidapp.computerPublicKey";
     public static final String PREFERENCES = "pt.ulisboa.tecnico.sirs.ssandroidapp.preferences";
     public static final String DEFAULT_UUID = "7e32b920-c98b-11e7-8f1a-0800200c9a66";
     public static final String PASSWORD_CHECK_STRING_ID = "pt.ulisboa.tecnico.sirs.ssandroidapp.checkString";
     public static final String PASSWORD_CHECK_STRING = "BANANANANANANANANA";
     public static final int RSA_KEY_SIZE = 2048;
-    public static final int RECEIVER_BUFFER_SIZE = 2048;
+    public static final int AES_KEY_SIZE = 256;
+    public static final int RECEIVER_BUFFER_SIZE = 4096;
     public static final String RSA_PUBLIC_BEGIN = "-----BEGIN PUBLIC KEY-----";
     public static final String RSA_PUBLIC_END = "-----END PUBLIC KEY-----";
     public static final String RSA_PRIVATE_BEGIN = "-----BEGIN RSA PRIVATE KEY-----";
@@ -21,4 +22,7 @@ public class Constants {
     public static final String AES_BEGIN = "-----BEGIN AES KEY-----";
     public static final String AES_END = "-----END AES KEY-----";
     public static final String PASSWORD_ID = "pt.ulisboa.tecnico.sirs.ssandroidapp.password";
+    public static final String ANDROID_PUBLIC_KEY_ID = "pt.ulisboa.tecnico.sirs.ssandroidapp.androidPublicKey";
+    public static final String ANDROID_PRIVATE_KEY_ID = "pt.ulisboa.tecnico.sirs.ssandroidapp.androidPublicKey";
+    public static final String ANDROID_MEK_ID = "pt.ulisboa.tecnico.sirs.ssandroidapp.MEK";
 }
