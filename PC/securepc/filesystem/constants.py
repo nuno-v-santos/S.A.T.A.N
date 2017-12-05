9e0aff000c3100fdc45c08e69625fc19121cc632
Tomás Cunha <tomas.cunha@tecnico.ulisboa.pt>
2017-12-05 01:15:20 +0000

    Change log dir

--- PC/securepc/filesystem/constants.py
+++ PC/securepc/filesystem/constants.py
@@ -3,6 +3,8 @@ import os
 from securepc.constants import APP_NAME
 
 LOG_NAME = 'secure_log.txt'
+
 BACKUP_DIR = appdirs.user_cache_dir(APP_NAME)
-LOG_DIR = appdirs.user_log_dir(APP_NAME)
+LOG_DIR = appdirs.user_config_dir(APP_NAME)
+
 LOG_PATH = os.path.join(LOG_DIR, LOG_NAME)
