d06a051857f8c2b3d70b08736c323bd811a0ab48
Tomás Cunha <tomas.cunha@tecnico.ulisboa.pt>
2017-12-05 01:44:25 +0000

    Add missing path to decrypted file key storage

--- PC/securepc/application.py
+++ PC/securepc/application.py
@@ -227,7 +227,7 @@ class _Application(object):
             async_publish('connected')
             self.communication.send(self.encrypted_file_key)
             self.decrypted_file_key = self.communication.receive(64)
-            AES256KeyManager().store_key(self.decrypted_file_key, self.password)
+            AES256KeyManager().store_key(self.decrypted_file_key, constants.DECRYPTED_FILE_KEY_PATH, self.password)
             self.decrypt_all()
             async_publish('decrypted')
 
